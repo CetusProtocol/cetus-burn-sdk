7aaffecda7c552a4b3b6c017dc08ba0941868ccd
KaiDonovan <anxia0818@proton.me>
2024-11-22 18:52:14 +0800

    fix update contract address

--- tests/data/init_mainnet_sdk.ts
+++ tests/data/init_mainnet_sdk.ts
@@ -8,7 +8,7 @@ export const burn_mainnet: SdkOptions = {
   },
   burn: {
     package_id: '0x12d73de9a6bc3cb658ec9dc0fe7de2662be1cea5c76c092fcc3606048cdbac27',
-    published_at: '0xb92ae17938cde6d856ee39c686d4cfb968c94155e59e24520fbf60de38ebcd21',
+    published_at: '0xb977b00649d3ab8950bcbbafb01fcf32e2e7718eb3133eff2e48c0cef04b1495',
     manager_id: '0x1d94aa32518d0cb00f9de6ed60d450c9a2090761f326752ffad06b2e9404f845',
     clmm_global_config: '0xdaa46292632c3c4d8f31f23ea0f9b36a28ff3677e9684980e4438403a67a3d8f',
     clmm_global_vault_id: '0xce7bceef26d3ad1f6d9b6f13a953f053e6ed3ca77907516481ce99ae8e588f2b',
--- tests/data/init_testnet_sdk.ts
+++ tests/data/init_testnet_sdk.ts
@@ -7,7 +7,7 @@ export const burn_testnet: SdkOptions = {
   },
   burn: {
     package_id: '0x3b494006831b046481c8046910106e2dfbe0d1fa9bc01e41783fb3ff6534ed3a',
-    published_at: '0x3b494006831b046481c8046910106e2dfbe0d1fa9bc01e41783fb3ff6534ed3a',
+    published_at: '0xaf89f8215c5b07eaac8b77c7745ce62f94cb76ef4bcb854e283f644c519ef43e',
     manager_id: '0xd04529ef15b7dad6699ee905daca0698858cab49724b2b2a1fc6b1ebc5e474ef',
     clmm_global_config: '0x9774e359588ead122af1c7e7f64e14ade261cfeecdb5d0eb4a5b3b4c8ab8bd3e',
     clmm_global_vault_id: '0xf78d2ee3c312f298882cb680695e5e8c81b1d441a646caccc058006c2851ddea',
